0ccd8fb98d958e4114e1c662f725e9df7e4468a1
Saitejahr <98202897+Saitejahr@users.noreply.github.com>
2023-05-17 16:53:31 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from 'react'
+import '../styles/App.css'
+import Form from './Form'
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <Form setAnswer={setAnswer} />
+        <h3 data-testid="answer">{answer}</h3>
+      </div>
+    )
+  }
 }
 
-
-export default App;
+export default App
