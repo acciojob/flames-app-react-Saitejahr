fbf2cb321c2fedda6c1d1a8c3f8c56a9576e9f8d
Saitejahr <98202897+Saitejahr@users.noreply.github.com>
2023-05-17 16:58:34 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component, useState } from 'react'
 import '../styles/App.css'
-import Form from './Form'
+import Form from './Form.js'
 
-class App extends Component {
-  render() {
-    return (
-      <div id="main">
-        {/* Do not remove the main div */}
-        <Form setAnswer={setAnswer} />
-        <h3 data-testid="answer">{answer}</h3>
-      </div>
-    )
-  }
+const App = () => {
+  const [answer, setAnswer] = useState('')
+
+  return (
+    <div id="main">
+      {/* Do not remove the main div */}
+      <Form setAnswer={setAnswer} />
+      <h3 data-testid="answer">{answer}</h3>
+    </div>
+  )
 }
 
 export default App
